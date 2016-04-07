f31e9de9b675d9bcb8a0ad5a8f90d94a530c659d
JiaJu Ni <jiajuni@JiaJus-MBP.local>
2016-04-07 13:22:19 -0500

    sync

--- spill/src/main.rs
+++ spill/src/main.rs
@@ -1,7 +1,10 @@
+extern crate regex;
+
 use std::fs::File;
 use std::io::{Read, Write};
 use std::env;
 use std::str::{Chars, SplitWhitespace};
+use regex::Regex;
 
 #[derive(Debug)]
 enum Instruction {
@@ -14,7 +17,7 @@ enum Instruction {
 enum Operation {
     Add,
     Sub,
-    Muliply,
+    Mul,
     And,
     Assign,
     ShiftLeft,
@@ -25,14 +28,13 @@ enum Operation {
 }
 
 #[derive(Debug)]
-enum Anything {
+enum Anything<'a> {
     Instruction(Instruction),
-    Register(String),
-    Number(String),
-    Label(String),
-    Operation(Operation),
-    Variable(String),
+    RawWord(&'a str),
 }
+
+// nightly rust feature
+// const fn label_regex: Regex = Regex::new(r"^:[a-zA-Z_][a-zA-Z_0-9]*$").unwrap();
 
 fn main() {
      // argument validation check
@@ -81,15 +83,48 @@ fn l1_instruction_parser(word_iter: &mut SplitWhitespace) -> Result<Instruction,
                     Err(e)  => return Err(e),
                 },
             ")" => break,
-            _   => words.push(Anything::RawWord(word.to_string())),
+            _   => words.push(Anything::RawWord(word)),
         }
     }
     println!("l1 parser {:?}", words);
 
+    match words.len() {
+        3 => {
+            match (&words[0], &words[1], &words[2]) {
+                (_, &Anything::RawWord("<-"), _) => println!("this is a match 3"),
+                _ => {}
+            }
+        }
+        _ => {}
+    }
 
     return Err("".to_string());
 }
 
-fn l1_word_parser(raw_word: &str) -> Anything {
+/*
+fn l1_word_parser(raw_word: &str) -> Result<Anything, String> {
     match raw_word {
-        "+="
+        "+=" => return Ok(Anything::Operation(Operation::Add)),
+        "<-" => return Ok(Anything::Operation(Operation::Assign)),
+        _    => return Err("Unknow operation".to_string()),
+    }
+
+    let label_regex = Regex::new(r"^:[a-zA-Z_][a-zA-Z_0-9]*$").unwrap();
+    let register_regex =
+        Regex::new(r"^(rdi|rsi|rdx|rcx|r8|r9|rax|rbx|rbp|r10|r11|r12|r13|r14|r15|rsp)$").unwrap();
+    let number_regex = Regex::new(r"^-?[0-9]+$").unwrap();
+    let var_regex = Regex::new(r"^[a-zA-Z_][a-zA-Z_0-9-]*$").unwrap();
+    
+    if register_regex.is_match(raw_word) {
+        return Ok(Anything::Register(raw_word.to_string()));
+    } else if label_regex.is_match(raw_word) {
+        return Ok(Anything::Label(raw_word.to_string()));
+    } else if number_regex.is_match(raw_word) {
+        return Ok(Anything::Number(raw_word.to_string()));
+    } else if var_regex.is_match(raw_word) {
+        return Ok(Anything::Variable(raw_word.to_string()));
+    } else {
+        return Err("[Syntax Error] Unknown Word".to_string());
+    }
+}
+*/
