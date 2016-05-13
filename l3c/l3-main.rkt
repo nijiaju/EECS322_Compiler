60a6095500e887ba22c6252cf300d55d78d368ef
Jiaju Ni <nijiaju@hotmail.com>
2016-05-12 19:08:12 -0500

    bug fix

--- l3c/l3-main.rkt
+++ l3c/l3-main.rkt
@@ -1,6 +1,6 @@
 #lang plai
 
-(require "L3-parser.rkt" "L3-compiler.rkt" "../l2c/l2-format.rkt")
+(require "l3-parser.rkt" "l3-compiler.rkt" "../l2c/l2-format.rkt")
 
 (require racket/cmdline)
 (define file-to-compile
@@ -9,6 +9,6 @@
    filename))
 
 ;========== MAIN ==========
-(define in (open-input-file "test/tests1.L3"))
+(define in (open-input-file file-to-compile))
 (define l3prog (l3-parsp (read in)))
 (display-prog (l3-compile-prog l3prog))
