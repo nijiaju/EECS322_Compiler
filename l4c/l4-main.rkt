671eea79110411d8954d5cbe8d3c6580b491eb84
Jiaju Ni <nijiaju@hotmail.com>
2016-05-15 05:28:18 -0500

    fix output

--- l4c/l4-main.rkt
+++ l4c/l4-main.rkt
@@ -2,14 +2,14 @@
 
 (require "l4-parser.rkt" "l4-compiler.rkt" "../l3c/l3-format.rkt")
 
-(require racket/cmdline)
-(define file-to-compile
-  (command-line
-   #:args (filename)
-   filename))
+;(require racket/cmdline)
+;(define file-to-compile
+;  (command-line
+;   #:args (filename)
+;   filename))
 
 ;========== MAIN ==========
-;(define in (open-input-file "test"))
-(define in (open-input-file file-to-compile))
+(define in (open-input-file "test"))
+;(define in (open-input-file file-to-compile))
 (define l4prog (l4-parsp (read in)))
-(format-l3-prog (l4-compile-prog l4prog))
+(displayln (format-l3-prog (l4-compile-prog l4prog)))
