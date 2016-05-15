0dc6aaf67b01a404b3e4f9cbd8b1ce77a72a2408
Jiaju Ni <nijiaju@hotmail.com>
2016-05-15 05:19:50 -0500

    add missing file

---
+++ l4c/l4-compiler-unit-test.rkt
@@ -0,0 +1,107 @@
+#lang plai
+
+(require rackunit "l4-compiler.rkt" "l4-parser.rkt" "../l3c/l3-format.rkt")
+(require "../l3c/l3-definition.rkt" "../l2c/l2-definition.rkt")
+
+(check-equal?
+ (l4-normalize (l4-parse '(if (f1 (f2 a) b) (f3) (f4 (f5 c d) (:f6 e f g)))))
+ (l3lete
+  (l3varia 'l3_x_0)
+  (l3funcal (l3varia 'f2) (list (l3varia 'a)))
+  (l3lete
+   (l3varia 'l3_x_1)
+   (l3funcal (l3varia 'f1) (list (l3varia 'l3_x_0) (l3varia 'b)))
+   (l3ife
+    (l3varia 'l3_x_1)
+    (l3defe (l3funcal (l3varia 'f3) '()))
+    (l3lete
+     (l3varia 'l3_x_2)
+     (l3funcal (l3varia 'f5) (list (l3varia 'c) (l3varia 'd)))
+     (l3lete
+      (l3varia 'l3_x_3)
+      (l3funcal
+       (l3label ':f6)
+       (list (l3varia 'e) (l3varia 'f) (l3varia 'g)))
+      (l3defe
+       (l3funcal
+        (l3varia 'f4)
+        (list (l3varia 'l3_x_2) (l3varia 'l3_x_3))))))))))
+
+(check-equal?
+ (l4-normalize (l4-parse '((let ([x (a b)]) (c x)) (d (let ([y e]) (f y))))))
+ (l3lete
+  (l3varia 'x)
+  (l3funcal (l3varia 'a) (list (l3varia 'b)))
+  (l3lete
+   (l3varia 'l3_x_4)
+   (l3funcal (l3varia 'c) (list (l3varia 'x)))
+   (l3lete
+    (l3varia 'y)
+    (l3value (l3varia 'e))
+    (l3lete
+     (l3varia 'l3_x_5)
+     (l3funcal (l3varia 'f) (list (l3varia 'y)))
+     (l3lete
+      (l3varia 'l3_x_6)
+      (l3funcal (l3varia 'd) (list (l3varia 'l3_x_5)))
+      (l3defe
+       (l3funcal (l3varia 'l3_x_4) (list (l3varia 'l3_x_6))))))))))
+
+(check-equal?
+ (l4-normalize (l4-parse '(+ a (- b c))))
+ (l3lete
+  (l3varia 'l3_x_7)
+  (l3biop (subop) (l3varia 'b) (l3varia 'c))
+  (l3defe (l3biop (addop) (l3varia 'a) (l3varia 'l3_x_7)))))
+
+(check-equal?
+ (l3func-l3fbody
+  (l4-compile-func
+   (l4-parsf '(:update (is_pass results)
+                       (new-tuple
+                        (let [(passed (aref results 0))]
+                          (if is_pass (+ 1 passed) passed))
+                        (+ 1 (aref results 1)))))))
+ (l3lete
+  (l3varia 'passed)
+  (l3arrref (l3varia 'results) (l3numbe 0))
+  (l3ife
+   (l3varia 'is_pass)
+   (l3lete
+    (l3varia 'l3_x_8)
+    (l3biop (addop) (l3numbe 1) (l3varia 'passed))
+    (l3lete
+     (l3varia 'l3_x_9)
+     (l3arrref (l3varia 'results) (l3numbe 1))
+     (l3lete
+      (l3varia 'l3_x_10)
+      (l3biop (addop) (l3numbe 1) (l3varia 'l3_x_9))
+      (l3defe
+       (l3newtup (list (l3varia 'l3_x_8) (l3varia 'l3_x_10)))))))
+   (l3lete
+    (l3varia 'l3_x_11)
+    (l3arrref (l3varia 'results) (l3numbe 1))
+    (l3lete
+     (l3varia 'l3_x_12)
+     (l3biop (addop) (l3numbe 1) (l3varia 'l3_x_11))
+     (l3defe
+      (l3newtup (list (l3varia 'passed) (l3varia 'l3_x_12)))))))))
+
+(check-equal?
+ (l4-normalize (l4-parse '(begin (+ a (- b c)) (e f g))))
+ (l3lete
+  (l3varia 'l3_x_13)
+  (l3biop (subop) (l3varia 'b) (l3varia 'c))
+  (l3lete
+   (l3varia 'l4_x_0)
+   (l3biop (addop) (l3varia 'a) (l3varia 'l3_x_13))
+   (l3defe (l3funcal (l3varia 'e) (list (l3varia 'f) (l3varia 'g)))))))
+
+(print (format-l3-prog
+        (l4-compile-prog
+         (l4-parsp '((:update)
+                     (:update (is_pass results)
+                              (new-tuple
+                               (let [(passed (aref results 0))]
+                                 (if is_pass (+ 1 passed) passed))
+                               (+ 1 (aref results 1)))))))))
---
+++ l4c/l4-compiler.rkt
@@ -0,0 +1,126 @@
+#lang plai
+
+(require "l4-definition.rkt" "../l3c/l3-definition.rkt" "../l3c/l3-parser.rkt")
+(require "l4-parser.rkt")
+
+(define/contract (l4-compile-prog p)
+  (-> l4prog? l3prog?)
+  (l3prog (l4-normalize (l4prog-l4entry p))
+          (map l4-compile-func (l4prog-l4funcl p))))
+
+(define/contract (l4-compile-func f)
+  (-> l4func? l3func?)
+  (l3func (l4func-l4fname f)
+          (l4func-l4fvarl f)
+          (length (l4func-l4fvarl f))
+          (l4-normalize (l4func-l4fbody f))))
+
+(define/contract (l4-normalize e)
+  (-> L4Expression? L3Expression?)
+  (l4-find e (l4noctxt)))
+
+(define/contract (l4-find e k)
+  (-> L4Expression? L4Context? L3Expression?)
+  (type-case L4Expression e
+    [l4let    (var valu body)
+              (l4-find valu (l4letctxt var body k))]
+    [l4if     (cond then else)
+              (l4-find cond (l4ifctxt then else k))]
+    [l4biop   (oper lhs rhs)
+              (l4-find lhs (l4bilctxt oper rhs k))]
+    [l4pred   (oper valu)
+              (l4-find valu (l4prdctxt oper k))]
+    [l4funcal (func argl)
+              (l4-find func (l4functxt argl k))]
+    [l4newarr (size valu)
+              (l4-find size (l4nasctxt valu k))]
+    [l4newtup (vall)
+              (l4-find (first vall) (l4ntpctxt empty (rest vall) k))]
+    [l4arrref (aray posi)
+              (l4-find aray (l4aractxt posi k))]
+    [l4arrset (aray posi valu)
+              (l4-find aray (l4asactxt posi valu k))]
+    [l4arrlen (aray)
+              (l4-find aray (l4alnctxt k))]
+    [l4print  (valu)
+              (l4-find valu (l4prtctxt k))]
+    [l4makecl (name vars)
+              (l4-find vars (l4mclctxt name k))]
+    [l4clproc (clos)
+              (l4-find clos (l4clpctxt k))]
+    [l4clvars (clos)
+              (l4-find clos (l4clvctxt k))]
+    [l4value  (valu)
+              (l4-fill (l3value (l4v-to-l3v valu)) k)]))
+
+(define/contract (l4-fill d k)
+  (-> L3Definition? L4Context? L3Expression?)
+  (type-case L4Context k
+    [l4letctxt (var body k)
+               (l3lete (l4v-to-l3v var) d (l4-find body k))]
+    [l4ifctxt  (then else k)
+               (l4-maybe-lift d (λ (v) (l3ife v (l4-find then k) (l4-find else k))))]
+    [l4bilctxt (oper rhs k)
+               (l4-maybe-lift d (λ (v) (l4-find rhs (l4birctxt oper v k))))]
+    [l4birctxt (oper lv k)
+               (l4-maybe-lift d (λ (v) (l4-fill (l3biop oper lv v) k)))]
+    [l4prdctxt (oper k)
+               (l4-maybe-lift d (λ (v) (l4-fill (l3pred oper v) k)))]
+    [l4functxt (argl k)
+               (if (l3value? d)
+                   (let ([f (unwrap-l3value d)])
+                     (if (empty? argl)
+                         (l4-fill (l3funcal f empty) k)
+                         (l4-find (first argl) (l4argctxt f empty (rest argl) k))))
+                   (let ([x (l3varia (var-suffix 'l3_x_ 'L3))])
+                     (if (empty? argl)
+                         (l3lete x d (l4-fill (l3funcal x empty) k))
+                         (l3lete x d (l4-find (first argl) (l4argctxt x empty (rest argl) k))))))]
+    [l4argctxt (func done left k)
+               (if (l3value? d)
+                   (let ([a (unwrap-l3value d)])
+                     (if (empty? left)
+                         (l4-fill (l3funcal func (reverse (cons a done))) k)
+                         (l4-find (first left) (l4argctxt func (cons a done) (rest left) k))))
+                   (let ([x (l3varia (var-suffix 'l3_x_ 'L3))])
+                     (if (empty? left)
+                         (l3lete x d (l4-fill (l3funcal func (reverse (cons x done))) k))
+                         (l3lete x d (l4-find (first left) (l4argctxt func (cons x done) (rest left) k))))))]
+    [l4nasctxt (valu k)
+               (l4-maybe-lift d (λ (v) (l4-find valu (l4navctxt v k))))]
+    [l4navctxt (size k)
+               (l4-maybe-lift d (λ (v) (l4-fill (l3newarr size v))))]
+    [l4ntpctxt (done left k)
+               (if (empty? left)
+                   (l4-maybe-lift d (λ (v) (l4-fill (l3newtup (reverse (cons v done))) k)))
+                   (l4-maybe-lift
+                    d (λ (v) (l4-find (first left) (l4ntpctxt (cons v done) (rest left) k)))))]
+    [l4aractxt (posi k)
+               (l4-maybe-lift d (λ (v) (l4-find posi (l4arpctxt v k))))]
+    [l4arpctxt (aray k)
+               (l4-maybe-lift d (λ (v) (l4-fill (l3arrref aray v) k)))]
+    [l4asactxt (posi valu k)
+               (l4-maybe-lift d (λ (v) (l4-find posi (l4aspctxt v valu k))))]
+    [l4aspctxt (aray valu k)
+               (l4-maybe-lift d (λ (v) (l4-find valu (l4asvctxt aray v k))))]
+    [l4asvctxt (aray posi k)
+               (l4-maybe-lift d (λ (v) (l4-fill (l3arrset aray posi v) k)))]
+    [l4alnctxt (k)
+               (l4-maybe-lift d (λ (v) (l4-fill (l3arrlen v) k)))]
+    [l4prtctxt (k)
+               (l4-maybe-lift d (λ (v) (l4-fill (l3printd v) k)))]
+    [l4mclctxt (name k)
+               (l4-maybe-lift d (λ (v) (l4-fill (l3makecl name v) k)))]
+    [l4clpctxt (k)
+               (l4-maybe-lift d (λ (v) (l4-fill (l3clproc v) k)))]
+    [l4clvctxt (k)
+               (l4-maybe-lift d (λ (v) (l4-fill (l3clvars v) k)))]
+    [l4noctxt  () (l3defe d)]))
+                     
+(define/contract (l4-maybe-lift d f)
+  (-> L3Definition? (-> L3Value? L3Expression?) L3Expression?)
+  (if (l3value? d)
+      (let ([v (unwrap-l3value d)])
+        (f v))
+      (let ([x (l3varia (var-suffix 'l3_x_ 'L3))])
+        (l3lete x d (f x)))))
